862f9548df598d466c68429a1cdf5e4348c517c1
ShikihaAkiRui <102493780+ShikihaAkiRui@users.noreply.github.com>
2023-04-14 15:59:20 +0900

    更新
    Vector3 作成
    次回、コンストラクタの初期化について聞くこと

---
+++ 00_aqua 7.17 GameObject/aqua/src/mathematics/vector/vector3/vector3.cpp
@@ -0,0 +1,104 @@
+/*
+	@file       vector3.cpp
+	@brief      三次元ベクトル
+
+*/
+
+#include "vector3.h"
+#include"../../../debug/debug.h"
+#include<cmath>
+
+const aqua::CVector3 aqua::CVector3::ZERO = aqua::CVector3(0.0f, 0.0f, 0.0f);
+const aqua::CVector3 aqua::CVector3::ONE = aqua::CVector3(1.0f, 1.0f, 1.0f);
+
+/*
+	コンストラクタ
+*/
+aqua::CVector3::
+CVector3(void)
+{
+}
+
+/*
+	コンストラクタ
+*/
+aqua::CVector3::
+CVector3(float x, float y, float z)
+{
+}
+
+/*
+	コンストラクタ
+*/
+aqua::CVector3::
+CVector3(const VECTOR& v)
+{
+}
+
+/*
+	正規化
+*/
+aqua::CVector3
+aqua::CVector3::
+Normalize(void)
+{
+	return Normalize(*this);
+}
+
+/*
+	正規化
+*/
+aqua::CVector3 aqua::CVector3::Normalize(const VECTOR& v)
+{
+	return VNorm(v);
+}
+
+/*
+	ベクトルの大きさ
+*/
+float
+aqua::CVector3::
+Length(void)
+{
+	return Length(*this);
+}
+
+/*
+	ベクトルの大きさ
+*/
+float
+aqua::CVector3::
+Length(const VECTOR& v)
+{
+	return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+/*
+	内積
+*/
+float
+aqua::CVector3::
+Dot(const VECTOR& vA, const VECTOR& vB)
+{
+	return VDot(vA,vB);
+}
+
+/*
+	外積
+*/
+aqua::CVector3 
+aqua::CVector3::
+Cross(const VECTOR& vA, const VECTOR& vB)
+{
+	return VCross(vA,vB);
+}
+
+/*
+	デバックログに出力する
+*/
+void
+aqua::CVector3::
+DebugLog(void)
+{
+	AQUA_DEBUG_LOG("Vector3(x:" + std::to_string(x) + ", y:" + std::to_string(y) + ", z:" + std::to_string(z) + ")");
+}
--- 00_aqua 7.17 GameObject/aqua/src/mathematics/vector/vector3/vector3.h
+++ 00_aqua 7.17 GameObject/aqua/src/mathematics/vector/vector3/vector3.h
@@ -19,6 +19,278 @@ namespace aqua
 		*/
 		CVector3(void);
 
+		/*
+			@brief	コンストラクタ
+
+			@param[in]	x	x成分
+			@param[in]	y	y成分
+			@param[in]	z	z成分
+		*/
+		CVector3(float x,float y,float z);
+
+		/*
+			@brief	コンストラクタ
+
+			@param[in]	v	ベクトル
+		*/
+		CVector3(const VECTOR& v);
+
+		/*
+			@brief	デストラクタ
+		*/
+		~CVector3(void) = default;
+
+		/*
+			@brief	ベクトルの正規化<br>
+					自身のベクトルも正規化される
+
+			@return	正規化されたベクトル
+		*/
+		CVector3	Normalize(void);
+
+		/*
+			@brief	ベクトルの正規化		か
+
+			@param[in]	v	ベクトルクラス
+
+			@return	正規化されたベクトル
+		*/
+		static CVector3	Normalize(const VECTOR& v);
+
+		/*
+			@brief	ベクトルの大きさ取得
+			
+			@return	ベクトルの大きさ
+		*/
+		float	Length(void);
+
+		/*
+			@brief	ベクトルの大きさ取得
+
+			@param[in]	v ベクトルクラス
+
+			@return	ベクトルの大きさ
+		*/
+		static float Length(const VECTOR& v);
+
+		/*
+			@brief	2つのベクトルの内積<br>
+
+			@param[in]	vA	ベクトルクラス	
+			@param[in]	vB	ベクトルクラス	
+
+			@return	内積
+		*/
+		static float Dot(const VECTOR& vA, const VECTOR& vB);
+
+		/*
+			@brief	2つのベクトルの外積<br>
+
+			@param[in]	vA	ベクトルクラス
+			@param[in]	vB	ベクトルクラス
+
+			@return	外積
+		*/
+		static CVector3 Cross(const VECTOR& vA, const VECTOR& vB);
+
+		/*
+			@brief	代入演算子のオーバーロード
+
+			@param[in]	v	ベクトルクラス
+
+			@return	ベクトルクラス
+		*/
+		CVector3& operator=(const VECTOR& v)
+		{
+			x = v.x;
+			y = v.y;
+			z = v.z;
+
+			return *this;
+		}
+
+		/*
+			@brief 加算演算子のオーバーロード
+
+			@param[in]	v	ベクトルクラス
+
+			@return　ベクトルクラス
+		*/
+		CVector3& operator+=(const VECTOR& v)
+		{
+			x += v.x;
+			y += v.y;
+			z += v.z;
+
+			return *this;
+		}
+
+		/*
+			@brief	減算演算子のオーバーロード
+
+			@param[in]	v	ベクトルクラス
+
+			@return	ベクトルクラス
+		*/
+		CVector3& operator-=(const VECTOR& v)
+		{
+			x -= v.x;
+			y -= v.y;
+			z -= v.z;
+
+			return *this;
+		}
+
+		/*
+			@brief	乗算演算子のオーバーロード
+
+			@param[in]	v	ベクトルクラス
+
+			@return	ベクトルクラス
+		*/
+		CVector3& operator*=(float scalar)
+		{
+			x *= scalar;
+			y *= scalar;
+			z *= scalar;
+
+			return *this;
+		}
+		
+		/*
+			@brief	等価演算子のオーバーロード
+
+			@param[in]	v ベクトルクラス
+
+			@return 二つのベクトルが等しい
+			@retval true	等しい
+			@retval false	等しくない
+		*/
+		bool	operator==(const VECTOR& v)const 
+		{
+			return(x == v.x && y == v.y && z == v.z);
+		}
+
+		/*
+			@brief	不等演算子のオーバーロード
+
+			@param[in] v ベクトルクラス
+
+			@return 二つのベクトルが等しくない
+			@retval	ture	等しくない
+			@retval	false	等しい
+		*/
+		bool	operator!=(const VECTOR& v)const
+		{
+			return(x != v.x || y != v.y || z != v.z);
+		}
+
+		/*
+			@brief	正符号演算子のオーバーロード
+
+			@param[in] v ベクトルクラス
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator+(const VECTOR& v) { return v; }
+
+		/*
+			@brief 負符号演算子のオーバーロード
+
+			@param[in]	v	ベクトルクラス
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator-(const VECTOR& v) { return CVector3(-v.x, -v.y, -v.z); }
+
+		/*
+			@brief	加算演算子のオーバーロード
+
+			@param[in]	vA	ベクトルクラス
+			@param[in]	vB	ベクトルクラス
+
+			@return	ベクトルクラス
+		*/
+		friend CVector3 operator+(const VECTOR& vA, const VECTOR& vB) { return CVector3(vA.x + vB.x, vA.y + vB.y, vA.z + vB.z); }
+
+		/*
+			@brief	減算演算子のオーバーロード
+
+			@param[in]	vA	ベクトルクラス
+			@param[in]	vB	ベクトルクラス
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator-(const VECTOR& vA, const VECTOR& vB) { return CVector3(vA.x - vB.x, vA.y - vB.y, vA.z - vB.z); }
+
+		/*
+			@brief  乗算演算子のオーバーロード
+
+			@param[in]	vA	ベクトルクラス
+			@param[in]	vB	ベクトルクラス
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator*(const VECTOR& vA, const VECTOR& vB) { return CVector3(vA.x * vB.x, vA.y * vB.y, vA.z * vB.z); }
+
+		/*
+			@brief  乗算演算子のオーバーロード
+
+			@param[in]	v	ベクトルクラス
+			@param[in]	scalar	スカラー値
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator*(const VECTOR& v, float scalar) { return CVector3(v.x * scalar, v.y * scalar, v.z * scalar); }
+
+		/*
+			@brief	乗算演算子のオーバーロード
+
+			@param[in]	scalar	スカラー値
+			@param[in]	v	ベクトルクラス
+
+			@return ベクトルクラス
+		*/
+		friend CVector3	operator*(float scalar, const VECTOR& v) { return CVector3(v.x * scalar, v.y * scalar, v.z * scalar); }
+
+		/*
+			@brief	除算演算子のオーバーロード
+
+			@param[in]	vA	ベクトルクラス
+			@param[in]	vB	ベクトルクラス
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator/(const VECTOR& vA, const VECTOR& vB) { return CVector3(vA.x / vB.x, vA.y / vB.y, vA.z / vB.z); }
+
+		/*
+			@brief	除算演算子のオーバーロード
+
+			@param[in]	v	ベクトルクラス
+			@param[in]	scalar	スカラー値
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator/(const VECTOR& v, float scalar) { return CVector3(v.x / scalar, v.y / scalar, v.z / scalar); }
+
+		/*
+			@brief	除算演算子のオーバーロード
+
+			@param[in]	scalar	スカラー値
+			@param[in]	v	ベクトルクラス
+
+			@return ベクトルクラス
+		*/
+		friend CVector3 operator/(float scalar, const VECTOR& v) { return CVector3(scalar / v.x, scalar / v.y, scalar / v.z); }
+
+		/*
+			@brief	デバックログに値を表示
+		*/
+		void DebugLog(void);
+
+		static const CVector3 ZERO;
+		static const CVector3 ONE;
+
 	};
 
 }
